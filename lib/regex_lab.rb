dafad2a83a2c3286b53f55d5588c03bd0d9a7ee4
Benis Tambe <will.jenifer@yahoo.com>
2021-01-23 10:56:13 -0500

    commit tree e0e3e8a470eb1c90cff9a766018bb9179f8274f6

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-
+    if word.scan(/[^aeiou]/)
+    return true
+    else
+        return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+text.match(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
